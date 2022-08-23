fca80b0035e1d18b58c1c9e06b834bb05020c169
biddy1618 <biddy.as.diddy@gmail.com>
2022-08-23 15:43:14 +0600

    Minor 3rd module exercises finished fix

---
+++ 3-module-exercises/4-exercise-fastAPI/main.py
@@ -0,0 +1,26 @@
+
+# Import Union since our Item object will have tags that can be strings or a list.
+from typing import Union 
+
+from fastapi import FastAPI
+# BaseModel from Pydantic is used to define data objects.
+from pydantic import BaseModel
+
+# Declare the data object with its components and their type.
+class TaggedItem(BaseModel):
+    name: str
+    tags: Union[str, list] 
+    item_id: int
+
+app = FastAPI()
+
+# This allows sending of data (our TaggedItem) via POST to the API.
+@app.post("/items/")
+async def create_item(item: TaggedItem):
+    return item
+
+# A GET that in this case just returns the item_id we pass, 
+# but a future iteration may link the item_id here to the one we defined in our TaggedItem.
+@app.get("/items/{item_id}")
+async def get_items(item_id: int, count: int = 1):
+    return {"fetch": f"Fetched {count} of {item_id}"}
---
+++ 3-module-exercises/5-exercise-fastAPI-test/test.main.py
@@ -0,0 +1,19 @@
+# test_bar.py
+
+import json
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_post():
+    data = json.dumps({"value": 10})
+    r = client.post("/42?query=5", data=data)
+    print(r.json())
+    assert r.json()["path"] == 42
+    assert r.json()["query"] == 5
+    assert r.json()["body"] == {"value": 10}
+
+
